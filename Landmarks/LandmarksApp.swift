598d78fec5883c611fd002515e344ecbfebd23af
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-02-20 12:11:08 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vladyslav Dikhtiaruk on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
